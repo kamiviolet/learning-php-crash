1dafb70999940a655af6b6ab7468849bd10673ae
kamiviolet <kamiviolet@gmail.com>
2023-07-09 11:31:19 +0100

    complete revision on 02_variables

--- _starter_files/02_variables.php
+++ _starter_files/02_variables.php
@@ -1,6 +1,22 @@
 <?php
 
 /* ----- Variables & Data Types ----- */
+$variable; //unintialised variable, not recommended but allowed
+$variable = "variable";
+
+define("CONSTANT", "I have a constant variable");
+echo CONSTANT;
+echo PHP_EOL;
+
+// By default, variables are always assigned by value. - instead of reference.
+// To assign by reference, simply prepend an ampersand (&) to the beginning of the variable which is being assigned (the source variable). For instance, the following code snippet outputs 'My name is Bob' twice: 
+
+$foo = 'Bob';              // Assign the value 'Bob' to $foo
+$bar = &$foo;              // Reference $bar to $foo('Bob' in this case)
+$bar = "My name is $bar";  // Alter $bar to a sentence containing its previous status ($foo - "Bob")
+echo $bar;
+echo $foo;                 // $foo is altered too. otherwise, $foo will not be affected. staying in 'Bob'
+
 
 /* --------- PHP Data Types --------- */
 /*
@@ -22,3 +38,100 @@
 - Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 - Variables are case-sensitive ($name and $NAME are two different variables)
 */
+
+// - String - A string is a series of characters surrounded by quotes
+
+$string_01 = 'string_01';
+$string_02 = "string_02";
+
+//heredoc will need to escape
+
+echo <<< STRING_03
+    string_03
+        declared in heredoc way;
+        \n
+STRING_03;
+
+//nowdoc will print everything literally without need to escape
+
+echo <<< 'STRING_04'
+    string_04
+        declared in nowdoc way;
+        \n
+STRING_04;
+
+echo $string_01;
+echo $string_02;
+echo PHP_EOL;
+
+// - Integer - Whole numbers
+// To explicitly convert a value to int, use either the (int) or (integer) casts.
+// If the string is numeric or leading numeric then it will resolve to the corresponding integer value, otherwise it is converted to zero (0). 
+// null is always converted to zero (0). 
+
+$integer = 1000;
+
+echo "integer: ";
+var_dump($integer);
+echo PHP_EOL;
+
+//interesting case -
+//numeric strings
+//https://www.php.net/manual/en/language.types.numeric-strings.php
+echo "5" + "5"; // not "55" but "10"
+echo PHP_EOL;
+
+// - Float - Decimal numbers
+
+$float = 1000.0001;
+
+echo "float: ";
+var_dump($float);
+echo PHP_EOL;
+
+// - Boolean - true or false, case-insensitive
+
+$bool = true;
+$bool_false = false; 
+
+var_dump($bool);
+echo $bool; //return 1
+echo PHP_EOL;
+
+var_dump($bool_false);
+echo $bool_false; //return nothing
+echo PHP_EOL;
+
+// To explicitly convert a value to bool, use the (bool) cast.
+
+var_dump((bool) "0");       // bool(false)
+var_dump((bool) 1);         // bool(true)
+var_dump((bool) -2);        // bool(true)
+echo PHP_EOL;
+echo ((bool) "0");       // bool(false)
+echo ((bool) 1);         // bool(true)
+echo ((bool) -2);        // bool(true)
+echo PHP_EOL;
+
+// - Array - An array is a special variable, which can hold more than one value
+// - Object - A class
+// - NULL - Empty variable
+//   undefined, and unset() variables will resolve to the value null. 
+// - Enumerations - a restricting layer on top of classes and class constants, intended to provide a way to define a closed set of possible values for a type. 
+// - Resource - A special variable that holds a resource
+// - Callables
+// - Mixed - equivalent to Any in TypeScript;
+// - Void - readonly
+// - Never - readonly
+
+/* 
+    To insert computed values into string, one can either use single-quotation marks or double-quotation marks.
+    - use '.' to concatenate strings and variables;
+    - use double-quotation marks like template literals in JS;
+ */
+echo 'string_01 is ' . $string_01 . 'and integer is ' . $integer;
+echo PHP_EOL;
+echo "string_2 is $string_02, and bool is $bool";
+
+//coercion in JS is equivalent to  type-juggling in php;
+//but its not really necessary as php will intepret according to context - operator of sort;
